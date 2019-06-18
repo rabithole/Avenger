3c689363e1c5416958e7e2df119069af77f4691a
Robert Kulp <pedalinon@gmail.com>
2019-06-17 20:01:30 -0700

    Furst commit

---
+++ src/components/AvengersList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AvengerList = props => {
+	return (
+		<div>
+			<h1>AvengerList</h1>
+		</div>
+	)
+}
+
+export default AvengerList;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = props => {
+	return (
+		<div>
+			<h2>Home</h2>
+			<img src='https://www.uncommongoods.com/images/category/fun-fullwidth.jpg' alt="nothgin" />
+
+			<button>Shop Now!</button>
+		</div>
+	)
+}
+
+export default Home;
