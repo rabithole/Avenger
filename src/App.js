2439d1783eb815c8ab5110a7fa544ef1730e83b6
Robert Kulp <pedalinon@gmail.com>
2019-06-24 22:24:36 -0700

    Using state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import AvengersList from './components/AvengersList';
 import Home from './components/Home';
-import ItemList from './components/ItemList';
 // import Avenger from './components/Avenger';
 import SingleAvenger from './components/SingleAvenger';
 
@@ -12,36 +11,49 @@ import { avengers } from './avengersData';
 
 import { Route } from 'react-router-dom';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      avengers: []
+    };
+  }
   // console.log(avengers)
-  return (
-    <div className="App">
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/AvengersList'>AvengersList</Link>
-      </nav>
 
-      <Route exact path='/' component={Home} />
+  componentDidMount() {
+    this.setState({ avengers: avengers });
+  }
 
-      <Route path='/ItemList' component={ItemList} />
-      <Route 
-        path='/SingleAvenger/:id' 
-        render={props => <SingleAvenger {...props} 
-        avenger={avengers}
-      /> }/>
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <NavLink to='/' exact>Home</NavLink>
+          <NavLink to='/AvengersList'>AvengersList</NavLink>
+        </nav>
 
-      <Route 
-        path='/AvengersList' 
-        render={props => <AvengersList {...props} 
-        avenger={avengers}
-      />}/>
-      
-      {/*
-      <Route path="/contact" component={Contact}/>
-      <Route path="/about" component={About}/>
-      */}
-    </div>
-  );
+        <Route exact path='/' component={Home} />
+
+        <Route 
+          path='/SingleAvenger/:id' 
+          render={props => <SingleAvenger {...props} 
+          avenger={this.state.avengers}
+        /> }/>
+
+        <Route 
+          path='/AvengersList' 
+          render={props => <AvengersList {...props} 
+          avenger={this.state.avengers}
+        />}/>
+
+        {/*
+        <Route path="/contact" component={Contact}/>
+        <Route path="/about" component={About}/>
+        */}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
